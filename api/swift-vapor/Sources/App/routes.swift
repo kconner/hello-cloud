c00620787df8d111f0683742908a4452bd418c50
Kevin Conner <connerk@gmail.com>
2020-04-23 23:22:49 -0400

    Create the /health-check route in swift-vapor api

--- api/swift-vapor/Sources/App/routes.swift
+++ api/swift-vapor/Sources/App/routes.swift
@@ -1,11 +1,7 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("health-check") { req -> String in
+        return "Health check OK"
     }
 }

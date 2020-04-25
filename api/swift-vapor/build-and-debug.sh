f99801c9fa70f74106b2c9f5e09e526b4c9d427b
Kevin Conner <connerk@gmail.com>
2020-04-25 00:55:08 -0400

    Establish hot reloading in the Swift/Vapor API implementation's debug container
    It only works half the time, and I don't know if that's a Docker thing or what.

--- api/swift-vapor/build-and-debug.sh
+++ api/swift-vapor/build-and-debug.sh
@@ -1,4 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash
 
-swift build --enable-test-discovery -c debug -Xswiftc -g
-.build/debug/Run serve --env development --hostname 0.0.0.0 --port 80
+read -r -d '' build_and_debug <<'EOF'
+sleep 0.2 \
+    && echo Rebuilding. \
+    && swift build --enable-test-discovery -c debug -Xswiftc -g \
+    && .build/debug/Run serve --env development --hostname 0.0.0.0 --port 80
+EOF
+
+while sleep 1 ; do
+    find Sources -name '*.swift' | entr -dr bash -c "$build_and_debug"
+done

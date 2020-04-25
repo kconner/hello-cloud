4aa96a1c5c781189247d820f4d5efde263e34111
Kevin Conner <connerk@gmail.com>
2020-04-25 11:55:15 -0400

    Enable entr's inotify / Docker for Mac workaround to make hot reloading work consistently

--- api/swift-vapor/watch-build-and-debug.sh
+++ api/swift-vapor/watch-build-and-debug.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+export ENTR_INOTIFY_WORKAROUND=1
+
 read -r -d '' build_and_debug <<'EOF'
-sleep 0.2 \
-    && echo Rebuilding. \
+echo Rebuilding. \
     && swift build --enable-test-discovery -c debug -Xswiftc -g \
     && .build/debug/Run serve --env development --hostname 0.0.0.0 --port 80
 EOF
 
 while sleep 1 ; do
-    find Sources -name '*.swift' | entr -dr bash -c "$build_and_debug"
+    find Sources | entr -dr sh -c "$build_and_debug"
 done

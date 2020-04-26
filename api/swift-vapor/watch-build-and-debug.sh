8b588c0f227a0a949bfcdb8321ef6b7f889d0023
Kevin Conner <connerk@gmail.com>
2020-04-26 11:47:18 -0400

    Using the swift server working group's guide, update debug and release build commands

--- api/swift-vapor/watch-build-and-debug.sh
+++ api/swift-vapor/watch-build-and-debug.sh
@@ -4,7 +4,7 @@ export ENTR_INOTIFY_WORKAROUND=1
 
 read -r -d '' build_and_debug <<'EOF'
 echo Rebuilding… \
-    && swift build --enable-test-discovery -c debug -Xswiftc -g \
+    && swift build --enable-test-discovery --sanitize=thread \
     && echo Serving. \
     && .build/debug/Run serve --env development --hostname 0.0.0.0 --port 80
 EOF

a7970b4597fd970969e3236ad3d213ecc41df891
Kevin Conner <connerk@gmail.com>
2020-04-28 23:58:56 -0400

    Try harder to stop the server when hot reloading fires

--- api/swift-vapor/build-and-debug.sh
+++ api/swift-vapor/build-and-debug.sh
@@ -1,13 +1,24 @@
-#!/bin/bash -e
+#!/bin/bash
 
 echo Rebuilding…
 swift build --enable-test-discovery --sanitize=thread
 
-existing_server_pid=$(pidof .build/debug/Run || true)
+# Look for an existing server process and kill it if it's running.
+# Vapor does not always seem to get terminated in response to SIGTERM.
+existing_server_pid=$(pidof .build/debug/Run)
 if [ -n "$existing_server_pid" ] ; then
-    echo 'Prior server did not shut down when sent SIGINT. Sending SIGKILL…'
-    kill -9 "$existing_server_pid"
+    echo 'Prior server did not shut down. Killing it.'
+    kill -KILL "$existing_server_pid"
 fi
 
+handleTerminate() { 
+  echo 'Terminating…' 
+  kill -TERM "$server_pid" 2>/dev/null
+}
+
+trap handleTerminate SIGTERM
+
 echo Serving.
-.build/debug/Run serve --env development --hostname 0.0.0.0 --port 80
+.build/debug/Run serve --env development --hostname 0.0.0.0 --port 80 &
+server_pid=$! 
+wait "$server_pid"
